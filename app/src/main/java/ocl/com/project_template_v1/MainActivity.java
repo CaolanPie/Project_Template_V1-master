31f2bb21406af412f467124ad8268a2aea757b2a
Caolan <caolan@ocluk.com>
2017-02-22 19:19:13 +0000

    start of listview for messages

--- app/src/main/java/ocl/com/project_template_v1/MainActivity.java
+++ app/src/main/java/ocl/com/project_template_v1/MainActivity.java
@@ -14,12 +14,20 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        populateMyList();
         setContentView(R.layout.activity_main);
     }
 
@@ -55,4 +63,34 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-}
+    private void populateMyList() {
+        ListView ll=(ListView) findViewById(R.id.list_messages);
+        String[] myStrings = { "First", "Second", "Third hard coded String"};
+
+        ArrayList<String> myArrayList = new ArrayList<String>(
+                Arrays.asList(myStrings));
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+
+
+
+        ll.setAdapter(adapter);
+/*
+        ll.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                //TODO Auto-generated method stub
+                TextView txt = (TextView) arg1;
+                System.out.println(txt.getText().toString());
+            }
+        }
+        ); */
+
+    } // End of populateMyList
+
+
+} // End of Class
+
+
+
