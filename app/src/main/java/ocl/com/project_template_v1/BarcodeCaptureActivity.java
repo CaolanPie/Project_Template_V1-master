46a423a58d6a3537e08c6357776b566e7039fd21
Caolan <caolan@ocluk.com>
2017-03-18 16:06:31 +0000

    completed barcode with working button 20170318_1606

--- app/src/main/java/ocl/com/project_template_v1/BarcodeCaptureActivity.java
+++ app/src/main/java/ocl/com/project_template_v1/BarcodeCaptureActivity.java
@@ -24,7 +24,19 @@ import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
 import java.io.IOException;
+
+import ocl.com.project_template_v1.ui.camera.GraphicOverlay;
+import ocl.com.project_template_v1.ui.camera.CameraSource;
+import ocl.com.project_template_v1.ui.camera.CameraSourcePreview;
+
 
 /**
  * Created by Caolán on 12/03/2017.
--- app/src/main/java/ocl/com/project_template_v1/ui/camera/CameraSourcePreview.java
+++ app/src/main/java/ocl/com/project_template_v1/ui/camera/CameraSourcePreview.java
@@ -10,6 +10,8 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.images.Size;
+
 import java.io.IOException;
 
 /**
