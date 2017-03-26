fd7241ba966dc63ce75abd81f55268956630b2f6
Caolan <caolan@ocluk.com>
2017-03-26 19:58:11 +0100

    fixing up the ListOfLists DBfunctions 20170326_1958

--- app/src/main/java/ocl/com/project_template_v1/DBfunctions/ListOfLists.java
+++ app/src/main/java/ocl/com/project_template_v1/DBfunctions/ListOfLists.java
@@ -12,25 +12,24 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 /*
- * This Class will hold the majority if not all the functions for the Lists database
+ * This Class will hold the majority if not all the functions for the ListOfLists database
  *      which will be used for accessing our data base
  *
  * Change Log
  * 26-Mar-17 Initial version, this may be all the DB functions we need.
  */
 
-public class Lists {
+public class ListOfLists {
 
-    private static final String DATABASE_NAME = "OCLDB";  // Our DB will be called OCLDB
-    private static final String DATABASE_TABLE = "Details"; // our table inside the db is called Details
+    private static final String DATABASE_NAME = "myListsDB";  // Our DB will be called myListsDB
+    private static final String DATABASE_TABLE = "Lists"; // our table inside the db is called Details
     private static final int DATABASE_VERSION = 1; // initial DB Version
 
     // These are the names of the columns in our table
     public static final String KEY_ROWID = "_id";
     public static final String KEY_Name = "Name";
-    public static final String KEY_Address = "Address";
-    public static final String KEY_DOB = "DOB";
-    public static final String KEY_Telephone = "Telephone";
+    public static final String KEY_DateCreated = "DateCreated";
+    public static final String KEY_LastEdited = "LastEdited";
 
 
     private DatabaseHelper mDbHelper;
@@ -40,23 +39,21 @@ public class Lists {
             "create table " + DATABASE_TABLE + " ("
                     + KEY_ROWID + " integer primary key autoincrement, "
                     + KEY_Name + " text not null, "
-                    + KEY_Address + " text not null, "
-                    + KEY_DOB + " text not null, "
-                    + KEY_Telephone + " text not null);";
+                    + KEY_DateCreated + " text not null, "
+                    + KEY_LastEdited + " text not null);";
 
     // above SQL statment translates to
-    // create table Details ( _id integer primary key autoincrement,
+    // create table ListOfLists ( _id integer primary key autoincrement,
     // 							Name text not null,
-    //							Address text not null,
-    //                          DOB text not null,
-    //							Telephone text not null);
+    //                          DateCreated text not null,
+    //							LastEdited text not null);
 	/*
 	The table would look like this (column names on top)
-	   _id	Name		Address			DOB			Telephone
-	   ===	===========	===============	=========== =========
-		1	Tony		51 Papworth		02-Jan-1962	077xx 123456
-		2	Caolam		12 Street		02-Feb-1962	077xx 123456
-		3	Another		45 Road			02-Apr-1962	077xx 123456
+	   _id	Name		DateCreated	LastEdited
+	   ===	===========	=========== =========
+		1	Work		02-Jan-2017	02-Jan-2017
+		2	LivingRoom	02-Feb-2017	02-Feb-2017
+		3	Upstairs	02-Apr-2017	02-Apr-2017
 
 	 */
 
@@ -88,7 +85,7 @@ public class Lists {
      * @return .mCtx
      */
 
-    public Lists(Context ctx) {
+    public ListOfLists(Context ctx) {
         this.mCtx = ctx;
     }
 
@@ -101,7 +98,7 @@ public class Lists {
      * throws SQLException - if it fails to open a DB it returns an exception.
      */
 
-    public Lists open() throws SQLException {
+    public ListOfLists open() throws SQLException {
         mDbHelper = new DatabaseHelper(mCtx);
         mDb = mDbHelper.getWritableDatabase();
         return this;
@@ -120,70 +117,67 @@ public class Lists {
     }
 
     /**
-     * Create a new Detail record using the supplied details (name, address etc...)
+     * Create a new ListOfLists record using the supplied details (name, address etc...)
      *
      * If the row is successfully created then the .insert will return the new rowId
      * for that row, otherwise return a -1 to indicate failure.
      *
      * @param name = name
-     * @param address = address
-     * @param DOB = date of birth
-     * @param telephone = telephone number
+     * @param DateCreated = date when the list was created
+     * @param LastEdited = date of when the user last edited this list
      *
      * @return rowId (if successful) or -1 if failed
      */
 
-    public long createDetailRow(String name,
-                                String address,
-                                String DOB,
-                                String telephone) {
+    public long createListOfListsRow(String name,
+                                String DateCreated,
+                                String LastEdited) {
         ContentValues initialValues = new ContentValues();
         initialValues.put(KEY_Name, name);
-        initialValues.put(KEY_Address, address);
-        initialValues.put(KEY_DOB, DOB);
-        initialValues.put(KEY_Telephone, telephone);
+        initialValues.put(KEY_DateCreated, DateCreated);
+        initialValues.put(KEY_LastEdited, LastEdited);
 
         return mDb.insert(DATABASE_TABLE, null, initialValues);
     }
 
     /**
-     * deletes selected Details row
+     * deletes selected ListOfLists row
      *
-     * input parameters rowId - the Details to delete (which row it is on)
+     * input parameters rowId - the Lists to delete (which row it is on)
      *
      * @return rowId
      */
 
-    public boolean deleteDetailRow(long rowId) {
+    public boolean deleteListOfListsRow(long rowId) {
         return
                 mDb.delete(DATABASE_TABLE, KEY_ROWID + "=" + rowId, null) > 0;
     }
 
     /**
-     * fetches ALL the Details from the table
+     * fetches ALL the ListOfLists from the table
      *
      * input parameters NONE
      *
-     * @return string[] - a list of all the reminders in our table
+     * @return string[] - a list of all the Lists in our table
      */
 
-    public Cursor fetchAllDetails() {
+    public Cursor fetchAllListOfLists() {
         return mDb.query(DATABASE_TABLE, new String[] {KEY_ROWID, KEY_Name,
-                KEY_Address, KEY_DOB, KEY_Telephone}, null, null, null, null, null);
+                KEY_DateCreated, KEY_LastEdited}, null, null, null, null, null);
     }
 
     /**
-     * Retrieves a SINGLE row of details based on the rowid
+     * Retrieves a SINGLE row of ListOfLists based on the rowid
      *
      * input parameters rowId - description not found
      *
-     * @return mCursor - a cursor pointing to the required Details Row
+     * @return mCursor - a cursor pointing to the required ListOfLists Row
      */
 
-    public Cursor fetchDetailRow(long rowId) throws SQLException {
+    public Cursor fetchListOfListsRow(long rowId) throws SQLException {
         Cursor mCursor =
                 mDb.query(true, DATABASE_TABLE, new String[] {KEY_ROWID,
-                                KEY_Name, KEY_Address, KEY_DOB, KEY_Address}, KEY_ROWID + "=" + rowId,
+                                KEY_Name, KEY_DateCreated, KEY_LastEdited}, KEY_ROWID + "=" + rowId,
                         null, null, null, null, null);
         if (mCursor != null) {
             mCursor.moveToFirst();
@@ -192,26 +186,24 @@ public class Lists {
     }
 
     /**
-     * saves changes to the detail row
+     * saves changes to the ListOfLists row
      *
      * input parameters rowId - the id of the row we want to update
-     * 				  title - the text of the new title
-     * 				  body - the text of the main body of the new updated reminder
-     * 				  reminderDateTime - the time and date of the new updated reminder
+     * 				  name - name of the list
+     * 				  DateCreated - date the list was created
+     * 				  LastEdited - date that the list was last edited
      *
      * @return true if update was successful
      */
 
-    public boolean updateDetailRow(long rowId,
+    public boolean updateListOfListsRow(long rowId,
                                    String name,
-                                   String address,
-                                   String DOB,
-                                   String telephone) {
+                                   String DateCreated,
+                                   String LastEdited) {
         ContentValues args = new ContentValues();
         args.put(KEY_Name, name);
-        args.put(KEY_Address, address);
-        args.put(KEY_DOB, DOB);
-        args.put(KEY_Telephone, telephone);
+        args.put(KEY_DateCreated, DateCreated);
+        args.put(KEY_LastEdited, LastEdited);
 
         return
                 mDb.update(DATABASE_TABLE, args, KEY_ROWID + "=" + rowId, null) > 0;
