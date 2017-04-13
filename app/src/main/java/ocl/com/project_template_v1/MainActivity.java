a7d5cd07fa9721c9794c427f5e9d1864cb41e535
Caolan <caolan@ocluk.com>
2017-04-13 06:12:32 +0100

    adding in spinner 20170412

--- app/src/main/java/ocl/com/project_template_v1/MainActivity.java
+++ app/src/main/java/ocl/com/project_template_v1/MainActivity.java
@@ -20,6 +20,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -91,6 +92,17 @@ public class MainActivity extends AppCompatActivity {
         */
 
         // GetAllLists();  // Get all records from my List of Items table
+
+        Spinner spinner = (Spinner) findViewById(R.id.planets_spinner);
+        spinner.setOnItemSelectedListener(this);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     } // end of onCreate
 
     /**
